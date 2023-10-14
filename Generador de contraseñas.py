abb572f382f7a4b65afa29c8b6a0c004c06e7e3b
A01711967 <142357020+A01711967@users.noreply.github.com>
2023-10-13 20:49:53 -0600

    Add files via upload

--- Generador de contraseñas.py
+++ Generador de contraseñas.py
@@ -3,47 +3,43 @@ import string
 
 def generar_contrasena():
     longitud = int(input("Longitud de la contraseña: "))
-    mayusculas = int(input("Cantidad de letras mayúsculas: "))
-    minusculas = int(input("Cantidad de letras minúsculas: "))
-    numeros = int(input("Cantidad de números: "))
-    especiales = int(input("Cantidad de caracteres especiales: "))
+    requisitos = {
+        "mayusculas": int(input("Cantidad de letras mayúsculas: ")),
+        "minusculas": int(input("Cantidad de letras minúsculas: ")),
+        "numeros": int(input("Cantidad de números: ")),
+        "especiales": int(input("Cantidad de caracteres especiales: "))
+    }
 
-    if longitud < mayusculas + minusculas + numeros + especiales:
+    if longitud < sum(requisitos.values()):
         print("La longitud de la contraseña debe ser mayor o igual a la suma de las cantidades especificadas.")
         return None
 
-    carac_may = string.ascii_uppercase
-    carac_min = string.ascii_lowercase
-    carac_num = string.digits
-    carac_esp = string.punctuation
+    caracteres = {
+        "mayusculas": list(string.ascii_uppercase),
+        "minusculas": list(string.ascii_lowercase),
+        "numeros": list(string.digits),
+        "especiales": list(string.punctuation)
+    }
 
-    if mayusculas > len(carac_may):
-        print("La cantidad de letras mayúsculas especificada es mayor de la disponible.")
-        return None
-    if minusculas > len(carac_min):
-        print("La cantidad de letras minúsculas especificada es mayor de la disponible.")
-        return None
-    if numeros > len(carac_num):
-        print("La cantidad de números especificada es mayor de la disponible.")
-        return None
-    if especiales > len(carac_esp):
-        print("La cantidad de caracteres especiales especificada es mayor de la disponible.")
-        return None
+    for clave, valor in requisitos.items():
+        if valor > len(caracteres[clave]):
+            print(f"La cantidad de {clave} especificada es mayor de la disponible.")
+            return None
 
     contrasenas = []
 
     for _ in range(4):
-        contrasena = ''.join(random.choice(carac_may) for _ in range(mayusculas))
-        contrasena += ''.join(random.choice(carac_min) for _ in range(minusculas))
-        contrasena += ''.join(random.choice(carac_num) for _ in range(numeros))
-        contrasena += ''.join(random.choice(carac_esp) for _ in range(especiales))
+        contrasena = []
+
+        for clave, valor in requisitos.items():
+            contrasena.extend(random.sample(caracteres[clave], valor))
 
         while len(contrasena) < longitud:
-            tipo_caracter = random.choice([carac_may, carac_min, carac_num, carac_esp])
-            contrasena += random.choice(tipo_caracter)
+            tipo_caracter = random.choice(list(caracteres.values()))
+            contrasena.append(random.choice(tipo_caracter))
 
-        contrasena = ''.join(random.sample(contrasena, len(contrasena)))
-        contrasenas.append(contrasena)
+        random.shuffle(contrasena)
+        contrasenas.append(''.join(contrasena))
 
     print("Contraseñas generadas:")
     for i, contrasena in enumerate(contrasenas):
